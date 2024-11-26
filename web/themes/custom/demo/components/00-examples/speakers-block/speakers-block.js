b7fe891074a8476a459bd4cccca183f3cb5a875c
Meeninath Dhobale <meeninath.dhobale@qed42.com>
2024-11-26 15:50:16 +0530

    Spacing updatinon in code

--- web/themes/custom/demo/components/00-examples/speakers-block/speakers-block.js
+++ web/themes/custom/demo/components/00-examples/speakers-block/speakers-block.js
@@ -1,13 +1,13 @@
-
 (function (Drupal) {
   'use strict';
 
   Drupal.behaviors.speakersBlock = {
     attach: function (context, settings) {
+      const splideElements = context.querySelectorAll(
+        '.speakers-carousel:not(.is-initialized)',
+      );
 
-      const splideElements = context.querySelectorAll('.speakers-carousel:not(.is-initialized)');
-      
-      splideElements.forEach(function(element) {
+      splideElements.forEach(function (element) {
         element.classList.add('is-initialized');
 
         const splide = new Splide(element, {
@@ -30,13 +30,11 @@
             },
             576: {
               perPage: 1,
-            }
+            },
           },
         });
 
-
         splide.mount();
-
 
         const prevArrow = element.querySelector('.splide__arrow--prev');
         const nextArrow = element.querySelector('.splide__arrow--next');
@@ -53,15 +51,12 @@
           });
         }
 
-
         splide.on('mounted moved', () => {
-
           if (splide.index === 0) {
             prevArrow.classList.add('is-disabled');
           } else {
             prevArrow.classList.remove('is-disabled');
           }
-
 
           if (splide.index === splide.length - splide.options.perPage) {
             nextArrow.classList.add('is-disabled');
@@ -73,45 +68,48 @@
 
       const modal = context.querySelector('#speaker-modal');
       const modalClose = modal?.querySelector('#modal-close');
-      const speakerCards = context.querySelectorAll('.speaker-card:not(.is-processed)');
+      const speakerCards = context.querySelectorAll(
+        '.speaker-card:not(.is-processed)',
+      );
 
-      speakerCards.forEach(card => {
+      speakerCards.forEach((card) => {
         card.classList.add('is-processed');
-        
-        card.addEventListener('click', function() {
+
+        card.addEventListener('click', function () {
           const data = this.dataset;
-          modal.style.display = "none";
+          modal.style.display = 'none';
           modal.querySelector('#modal-image').src = data.image;
           modal.querySelector('#modal-name').innerHTML = data.name;
-          modal.querySelector('#modal-designation').innerHTML = data.designation;
+          modal.querySelector('#modal-designation').innerHTML =
+            data.designation;
           modal.querySelector('#modal-company').innerHTML = data.organization;
           modal.querySelector('#modal-bio').innerHTML = data.bio;
           modal.querySelector('#social-drupal').href = data.socialDrupal.trim();
           modal.querySelector('#social-x').href = data.socialX.trim();
-          modal.querySelector('#social-linkedin').href = data.socialLinkedin.trim();
-          
-          modal.classList.add('is-active');
-          modal.style.display = "block";
+          modal.querySelector('#social-linkedin').href =
+            data.socialLinkedin.trim();
 
+          modal.classList.add('is-active');
+          modal.style.display = 'block';
         });
       });
 
       if (modalClose) {
-        modalClose.addEventListener('click', function() {
+        modalClose.addEventListener('click', function () {
           modal.classList.remove('is-active');
           document.body.style.overflow = 'auto';
-          modal.style.display = "none";
+          modal.style.display = 'none';
         });
       }
 
       if (modal) {
-        modal.addEventListener('click', function(event) {
+        modal.addEventListener('click', function (event) {
           if (event.target === modal) {
             modal.classList.remove('is-active');
             document.body.style.overflow = 'auto';
           }
         });
       }
-    }
+    },
   };
 })(Drupal);
